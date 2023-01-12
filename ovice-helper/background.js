f251ab2098e9969af4b619c8e9fe571ae3019a62
tanaka.shunsuke <tanaka.shunsuke@digitalvalue.co.jp>
2023-01-12 19:11:45 +0900

    add content_script

--- ovice-helper/background.js
+++ ovice-helper/background.js
@@ -1,43 +1,7 @@
 chrome.runtime.onInstalled.addListener(startUp);
 chrome.runtime.onStartup.addListener(startUp);
-chrome.action.onClicked.addListener((tab) => {
-    execute('ovice-helper', tab.id, tab.url);
-});
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    execute(info.menuItemId, tab.id, tab.url);
-});
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log(123);
-});
 
 async function startUp() {
     try {
-        const tab = await getCurrentTab();
-        const injectionResults = chrome.scripting.executeScript({
-            target: { tab: tab.id },
-            func: startUpScript,
-            args: [],
-        });
-        for (const frameResult of injectionResults) {
-            console.log('Frame Title: ' + frameResult.result);
-        }
     } catch (e) {}
-}
-
-function execute(menuItemId, tabId, url) {
-    try {
-        chrome.scripting.executeScript({
-            target: { tabId },
-            func: executeScript,
-            args: [menuItemId, tabId, url],
-        });
-    } catch (e) {}
-}
-
-function startUpScript() {
-    console.log(window.location.href);
-}
-
-function executeScript(menuItemId, tabId, url) {
-    console.log(menuItemId, tabId, url);
 }
---
+++ ovice-helper/content_script.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
+        addButtons('exclamation.png', 'exclamation.gif', '87-88-17ok-hand.png');
+        addButtons('question.png', 'question.gif', '111ared-heart.png');
+        addButtons('sweat.png', 'sweat.gif', '111ared-heart.png');
+        addButtons('kusa.png', 'kusa.gif', 'drum.png');
+    }, 1000);
+});
+
+function addButtons(buttonIcon, image, soundImageId) {
+    const menuBar = document.querySelector('#MenuBar > div:nth-of-type(2)');
+    menuBar.insertAdjacentHTML(
+        'beforeend',
+        `<button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium ext-btn-style" tabindex="0" type="button" aria-label="reaction" onclick="
+    ovice.chat(
+      '<div class=&quot;break-space&quot;><img width=&quot;20&quot; height=&quot;20&quot; id=&quot;${soundImageId}&quot; style=&quot;margin:&quot; src=&quot;//raw.githubusercontent.com/snst-lab/chrome-extensions/main/ovice-helper/image/${image}&quot; /></div>'
+    );
+    ovice.commitReaction();
+    "><img width="24" height="24" src="//raw.githubusercontent.com/snst-lab/chrome-extensions/main/ovice-helper/icon/${buttonIcon}"></button>`
+    );
+}
